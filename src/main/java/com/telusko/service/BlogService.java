02598a44309c90d46094ea471987bb29d5a6b869
Gaurav Sharma <gsharma1154@gmail.com>
2024-08-05 01:49:47 +0530

    intial commit

---
+++ src/main/java/com/telusko/service/BlogService.java
@@ -0,0 +1,14 @@
+package com.telusko.service;
+
+import com.telusko.model.Blog;
+
+import java.util.List;
+
+public interface BlogService {
+
+    List<Blog> getAllBlogs();
+
+    Blog getBlogById(int id);
+
+    Blog addBlog(Blog blog);
+}
